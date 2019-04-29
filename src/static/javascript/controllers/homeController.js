e0843ebf3ee5957994fe23493e3c8dd535f505fe
Belberus <almartmen@gmail.com>
2019-04-29 12:26:37 +0200

    #3 #12 Show frame in canvas. Basic controls of key-point creation

--- src/static/javascript/controllers/homeController.js
+++ src/static/javascript/controllers/homeController.js
@@ -2,48 +2,19 @@ angular.module('CVGTool')
 
     .controller('homeCtrl', ['$scope', '$state', '$interval', '$mdDialog', 'homeSrvc', function ($scope, $state, $interval, $mdDialog, homeSrvc) {
 
-      // Variables to control the for camera views
-      $scope.cameraViewSelected = "";
-      $scope.isCameraViewSelected = false;
+//////// TOOLS
+      $scope.mode = 'navigation';  // navigation = Normal
+                                    // keypoint = Key-Point mode
 
-      // Variables to control the loaded cameras array
-      $scope.cameraSelected = "";
-      $scope.isCameraSelected = true;
-      $scope.loadedCameras = [];
-      $scope.recommendedFrames = "";
+      $scope.switchMode = function (newMode) {
+        $scope.mode = newMode;
+      }
 
+//////// TIMELINE
       // Variables to control the timeline
       $scope.isPlaying = false;
       var promise;
 
-      // TODO: Check if this thing works
-      function fitCanvasToContainer(canvas){
-        // Make it visually fill the positioned parent
-        canvas.style.width ='100%';
-        canvas.style.height='100%';
-        // ...then set the internal size to match
-        // canvas.width  = canvas.offsetWidth;
-        canvas.height = canvas.offsetHeight;
-
-        canvas.width = canvas.height * (canvas.clientWidth / canvas.clientHeight);
-      }
-
-      var canvas1 = document.getElementById('canvas1');
-      var ctx1 = canvas1.getContext('2d');
-      var canvas2 = document.getElementById('canvas2');
-      var ctx2 = canvas2.getContext('2d');
-      var canvas3 = document.getElementById('canvas3');
-      var ctx3 = canvas3.getContext('2d');
-      var canvas4 = document.getElementById('canvas4');
-      var ctx4 = canvas4.getContext('2d');
-
-
-      fitCanvasToContainer(canvas1);
-      fitCanvasToContainer(canvas2);
-      fitCanvasToContainer(canvas3);
-      fitCanvasToContainer(canvas4);
-
-      // TIMELINE
       $scope.slider = {   // Options and values for the slider
         value: 1,
         options: {
@@ -85,7 +56,26 @@ angular.module('CVGTool')
         }
       }
 
-      $scope.imageTest;
+//////// CAMERAS
+      // Variables to control the for camera views
+      $scope.cameraViewSelected = "";
+      $scope.isCameraViewSelected = false;
+
+      // Variables to control the loaded cameras array
+      $scope.cameraSelected = "";
+      $scope.isCameraSelected = true;
+      $scope.loadedCameras = [];
+      $scope.recommendedFrames = "";
+
+      /*
+       * Structure:
+       *    loadedCameras:
+       *        cameraX:
+       *            arrayOfFrames: []
+       *                  frameY: image, key-points: []
+       */
+
+
       // Function that opens the dialog in charge of adding a new camera
       $scope.addCamera = function() {
           $mdDialog.show({
@@ -102,5 +92,82 @@ angular.module('CVGTool')
           );
       }
 
+//////// CANVASES
+      $scope.canvases = {   // Initial canvas structure
+        canvas1:{
+          element: null,
+          ctx: null,
+          valid: false,     // when set to true, the canvas will redraw everything
+          dragging: false,  // Keep track of when we are dragging
+          dragoffx: 0,
+          dragoffy: 0,
+        },
+        canvas2:{
+          element: null,
+          ctx: null,
+          valid: false,     // when set to true, the canvas will redraw everything
+          dragging: false,  // Keep track of when we are dragging
+          dragoffx: 0,
+          dragoffy: 0,
+        },
+        canvas3:{
+          element: null,
+          ctx: null,
+          valid: false,     // when set to true, the canvas will redraw everything
+          dragging: false,  // Keep track of when we are dragging
+          dragoffx: 0,
+          dragoffy: 0,
+        },
+        canvas4:{
+          element: null,
+          ctx: null,
+          valid: false,     // when set to true, the canvas will redraw everything
+          dragging: false,  // Keep track of when we are dragging
+          dragoffx: 0,
+          dragoffy: 0,
+        },
+      }
+
+      function fitToContainer(canvas){
+        // Make it visually fill the positioned parent
+        canvas.style.width ='100%';
+        canvas.style.height='100%';
+        // ...then set the internal size to match
+        canvas.width  = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+      }
+
+
+      $scope.setCanvases = function() {
+        var canvas1 = document.getElementById('canvas1');
+        var ctx1 = canvas1.getContext('2d');
+        fitToContainer(canvas1)
+
+        $scope.canvases.canvas1.element = canvas1;
+        $scope.canvases.canvas1.ctx = ctx1;
+
+        var canvas2 = document.getElementById('canvas2');
+        var ctx2 = canvas2.getContext('2d');
+        fitToContainer(canvas2)
+
+        $scope.canvases.canvas2.element = canvas2;
+        $scope.canvases.canvas2.ctx = ctx2;
+
+        var canvas3 = document.getElementById('canvas3');
+        var ctx3 = canvas3.getContext('2d');
+        fitToContainer(canvas3)
+
+        $scope.canvases.canvas3.element = canvas3;
+        $scope.canvases.canvas3.ctx = ctx3;
+
+        var canvas4 = document.getElementById('canvas4');
+        var ctx4 = canvas4.getContext('2d');
+        fitToContainer(canvas4)
+
+        $scope.canvases.canvas4.element = canvas4;
+        $scope.canvases.canvas4.ctx = ctx4;
+      }
+
+      $scope.setCanvases();
 
 }]);
