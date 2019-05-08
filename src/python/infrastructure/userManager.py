3238d4f1495b95898f2abf3537001f3d8f00b9d8
beapc18 <beatrix.pc@gmail.com>
2019-05-08 12:26:41 +0200

    #29 Add passwords for users. Get/create/update/remove users

--- src/python/infrastructure/userManager.py
+++ src/python/infrastructure/userManager.py
@@ -1,4 +1,8 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, errors
+import logging
+
+# UserService logger
+log = logging.getLogger('userManager')
 
 class UserManager:
 
@@ -6,10 +10,78 @@ class UserManager:
     db = c.cvg
     collection = db.user
 
-    # Return info user if exist in DB. Ignore mongo id and pwd
-    def getUser(this, user, pwd):
-        result = this.collection.find_one({"name": user, "password": pwd}, {"_id": 0, "password": 0 })
-        if result == None:
+    # Return info user if exist user-pwd in DB. Ignore mongo id and pwd
+    def getUserPwd(this, user, pwd):
+        try:
+            result = this.collection.find_one({"name": user, "password": pwd}, {"_id": 0, "password": 0 })
+            if result == None:
+                return 'Error'
+            else:
+                return result
+        except errors.PyMongoError as e:
+            log.exception('Error finding login user in db')
             return 'Error'
-        else:
-            return result
+
+    # Return info user if exist in DB. Ignore mongo id and pwd
+    def getUser(this, user):
+        try:
+            result = this.collection.find_one({"name": user}, {"_id": 0, "password": 0 })
+            if result == None:
+                return 'Error'
+            else:
+                return result
+        except errors.PyMongoError as e:
+            log.exception('Error finding user in db')
+            return 'Error'
+
+    # Return list with info of all users. Empty list if there are no users
+    # Ignore mongo id and pwd
+    def getUsers(this):
+        try:
+            result = this.collection.find({},{"_id": 0, "password": 0 })
+            return list(result)
+        except errors.PyMongoError as e:
+            log.exception('Error finding users in db')
+            return 'Error'
+
+    # Return 'ok' if the user has been created
+    def createUser(this, user, pwd, assignedTo, role, email):
+        try:
+            result = this.collection.insert_one({"name": user, "password": pwd, \
+                    "assignedTo": assignedTo, "role": role, "email": email })
+            if result.acknowledged:
+                return 'ok'
+            else:
+                return 'Error'
+        except errors.PyMongoError as e:
+            log.exception('Error creating user in db')
+            return 'Error'
+
+    # Return 'ok' if the user has been updated. if user doesn't exist, it isn't created
+    def updateUser(this, user, pwd, assignedTo, role, email):
+        query = {"name": user}      # Search by user name
+        # Update values (pwd, assignedTo, role, email)
+        newValues = {"$set": {"password":pwd, "assignedTo":assignedTo, "role":role, "email": email}}
+        try:
+            result = this.collection.update_one(query, newValues, upsert=False)
+            # print(list(result))
+            if result.modified_count == 1:
+                return 'ok'
+            else:
+                return 'Error'
+        except errors.PyMongoError as e:
+            log.exception('Error updating user in db')
+            return 'Error'
+
+
+    # Return 'ok' if the user has been removed
+    def removeUser(this, user):
+        try:
+            result = this.collection.delete_one({"name": user})
+            if result.deleted_count == 1:
+                return 'ok'
+            else:
+                return 'Error'
+        except errors.PyMongoError as e:
+            log.exception('Error removing user in db')
+            return 'Error'
