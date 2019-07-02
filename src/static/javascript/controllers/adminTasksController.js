6d93897f164f79fde4fc8fba30ab719be17d6454
Belberus <almartmen@gmail.com>
2019-07-02 14:45:22 +0200

    #13, #12 integration of AIK library. Reformat of code. A bit of work on #27, task creation and management

--- src/static/javascript/controllers/adminTasksController.js
+++ src/static/javascript/controllers/adminTasksController.js
@@ -1,5 +1,41 @@
 angular.module('CVGTool')
 
 .controller('adminTasksCtrl', ['$scope', '$state', '$interval', '$mdDialog', 'adminTasksSrvc', 'navSrvc', function($scope, $state, $interval, $mdDialog, adminTasksSrvc, navSrvc) {
-    console.log("eh")
+    $scope.listOfUsers = []; // List of existing users
+    $scope.listOfDatasets = []; // List of existing datasets
+
+    $scope.activeDataset = navSrvc.getActiveDataset().name; // Current dataset
+
+    $scope.user = navSrvc.getUser(); // Get current user
+
+    $scope.newTask = { // Skeleton of task creation
+        name: "",
+        dataset: "",
+        user: ""
+    };
+
+    // Callback function for retrieving avaiable users
+    var callbackSuccessGetAvaiableUsers = function(users) {
+        $scope.listOfUsers = users;
+    }
+
+    // Function that retrieves all the users form the selected dataset
+    $scope.getAvaiableUsers = function() {
+        adminTasksSrvc.getUsersByDataset($scope.newTas.dateset, "user", callbackSuccessGetAvaiableUsers)
+    }
+
+    // Function to update the list of datasets (the variable)
+    var callbackGetDatasets = function(datasets) {
+        $scope.listOfDatasets = [];
+        for (let i = 0; i < datasets.length; i++) {
+            $scope.listOfDatasets.push({
+                "name": datasets[i].name
+            })
+        }
+    };
+
+    $scope.getDatasets = function() {
+        adminTasksSrvc.getDatasets(callbackGetDatasets);
+    }
+
 }]);
--- src/static/javascript/services/adminTasksService.js
+++ src/static/javascript/services/adminTasksService.js
@@ -1,5 +1,36 @@
 angular.module('CVGTool')
 
 .factory('adminTasksSrvc', function($state, $http, $httpParamSerializer) {
+    return {
+        getUsersByDataset: function(dataset, role, callbackSuccess) {
+            $http({
+                method: 'GET',
+                url: '/api/user/getUsersByDataset',
+                headers: {
+                    dataset: dataset,
+                    role: role
+                }
+            }).then(function successCallback(response) {
+                if (response.data.msg.length === 0) {
+                    callbackSuccess([])
+                } else {
+                    callbackSuccess(response.data.msg)
+                }
+            }, function errorCallBack(response) {
+                console.log(response.data.msg)
+            });
+        },
 
+        getDatasets: function(callback) {
+            $http({
+                method: 'GET',
+                url: '/api/dataset/getDatasets'
+            }).then(function successCallback(response) {
+                callback(response.data.msg);
+                console.log("Successfully retrieved list of users")
+            }, function errorCallback(response) {
+                console.log(response.data.msg)
+            });
+        },
+    }
 });
