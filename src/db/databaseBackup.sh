a6ab1f2cfb448805327319a73788eaaa06502eb7
beapc18 <beatrix.pc@gmail.com>
2019-10-08 09:45:13 +0200

    #125 Remove backups older than 7 days

--- src/db/databaseBackup.sh
+++ src/db/databaseBackup.sh
@@ -4,6 +4,4 @@ mongodump --host "172.18.0.2:27017" --out /home/cvg_anno/Data/datasets/actions_i
 
 find /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups -mtime +7 -delete
 
-#https://www.linuxtotal.com.mx/?cont=info_admon_006
-#https://unix.stackexchange.com/questions/194863/delete-files-older-than-x-days
 
