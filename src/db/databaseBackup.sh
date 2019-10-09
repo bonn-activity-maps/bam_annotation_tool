f37b31f70042b1f23847ea6bb2cd3276aea58b60
beapc18 <beatrix.pc@gmail.com>
2019-10-09 10:32:50 +0200

    #141 Add npm dependencies and update angular version

--- src/db/databaseBackup.sh
+++ src/db/databaseBackup.sh
@@ -2,6 +2,6 @@
 
 mongodump --host "172.18.0.2:27017" --out /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups/$(date +"%Y-%m-%d_%H:%M:%S") --db cvg --forceTableScan
 
-find /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups -mtime +7 -delete
+find /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups -mtime +6 -delete
 
 
