0a84bcae814a397d9431c32f62a10ff5491a7d8b
Belberus <almartmen@gmail.com>
2019-10-24 15:34:52 +0200

    #165 Frame range for interpolation increased to 15

--- src/static/javascript/controllers/toolController.js
+++ src/static/javascript/controllers/toolController.js
@@ -14,7 +14,7 @@ angular.module('CVGTool')
         $scope.frameJumpNumber = 1;
         $scope.frameJumpNumberOptions = [{ id: 1, tag: "1" }, { id: 2, tag: "2" }, { id: 3, tag: "3" }, { id: 4, tag: "4" }, { id: 5, tag: "5" }, { id: 6, tag: "6" }, { id: 7, tag: "7" }, { id: 8, tag: "8" }, { id: 9, tag: "9" }, { id: 10, tag: "10" }];
         $scope.activeDataset = navSrvc.getActiveDataset(); // Get the active dataset information
-
+        $scope.interpolationRange = 15;
         /////////
         // END OF INITIALIZE STARTING VARIABLES
         /////////
@@ -1815,7 +1815,7 @@ angular.module('CVGTool')
                 $scope.objectManager.objectTypes[objectType.toString()].objects[$scope.objectManager.selectedObject.uid.toString()] :
                 $scope.objectManager.objectTypes[objectType.toString()].objects[objectUid.toString()];
             var frameFrom = null;
-            for (var i = frameTo - 1; i >= Math.max($scope.isPosetrack() ? 0 : 1, frameTo - 7); i--) {
+            for (var i = frameTo - 1; i >= Math.max($scope.isPosetrack() ? 0 : 1, frameTo - $scope.interpolationRange); i--) {
                 if (object.frames[i - $scope.frameFrom].keypoints.length > 0) {
                     frameFrom = i;
                     break;
