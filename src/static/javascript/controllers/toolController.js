1fbbdb4a4d0271fd043cf241f43c52f69cd79e4f
dario <dari1495@gmail.com>
2019-10-24 10:54:16 +0200

    Update added more colours, closes #161. Add pt merge file just in case.

--- src/static/javascript/controllers/toolController.js
+++ src/static/javascript/controllers/toolController.js
@@ -1030,9 +1030,23 @@ angular.module('CVGTool')
                                     for (obj in objects) {
                                         var keypoints = objects[obj].frames[$scope.slider.value - $scope.frameFrom].keypoints;
                                         if ($scope.hasAnnotation(keypoints)) {
-                                            let colors = ["red", "blue", "yellow", "purple", "brown", "white", "grey", "cyan",
-                                                "pink", "lilac", "orange", "canary"
-                                            ];
+                                            let colors = [
+                                                "#63b598", "#ce7d78", "#ea9e70", "#a48a9e", "#c6e1e8", "#648177" ,"#0d5ac1" ,
+                                                "#f205e6" ,"#1c0365" ,"#14a9ad" ,"#4ca2f9" ,"#a4e43f" ,"#d298e2" ,"#6119d0",
+                                                "#d2737d" ,"#c0a43c" ,"#f2510e" ,"#651be6" ,"#79806e" ,"#61da5e" ,"#cd2f00" ,
+                                                "#9348af" ,"#01ac53" ,"#c5a4fb" ,"#996635","#b11573" ,"#4bb473" ,"#75d89e" ,
+                                                "#2f3f94" ,"#2f7b99" ,"#da967d" ,"#34891f" ,"#b0d87b" ,"#ca4751" ,"#7e50a8" ,
+                                                "#c4d647" ,"#e0eeb8" ,"#11dec1" ,"#289812" ,"#566ca0" ,"#ffdbe1" ,"#2f1179" ,
+                                                "#935b6d" ,"#916988" ,"#513d98" ,"#aead3a", "#9e6d71", "#4b5bdc", "#0cd36d",
+                                                "#250662", "#cb5bea", "#228916", "#ac3e1b", "#df514a", "#539397", "#880977",
+                                                "#f697c1", "#ba96ce", "#679c9d", "#c6c42c", "#5d2c52", "#48b41b", "#e1cf3b",
+                                                "#5be4f0", "#57c4d8", "#a4d17a", "#225bc8", "#be608b", "#96b00c", "#088baf",
+                                                "#f158bf", "#e145ba", "#ee91e3", "#05d371", "#5426e0", "#4834d0", "#802234",
+                                                "#6749e8", "#0971f0", "#8fb413", "#b2b4f0", "#c3c89d", "#c9a941", "#41d158",
+                                                "#fb21a3", "#51aed9", "#5bb32d", "#807fcb", "#21538e", "#89d534", "#d36647",
+                                                "#7fb411", "#0023b8", "#3b8c2a", "#986b53", "#f50422", "#983f7a", "#ea24a3",
+                                                "#79352c", "#521250", "#c79ed2", "#d6dd92", "#e33e52", "#b2be57", "#fa06ec",
+                                                "#1bb699", "#6b2e5f", "#64820f", "#1c27c1", "#21538e", "#89d534", "#d36647"];
                                             var imageCoords1 = this.toImage(keypoints[0]);
                                             var imageCoords2 = this.toImage(keypoints[1]);
                                             var width = Math.abs(imageCoords2[0] - imageCoords1[0]);
