89eb4c88f0af16884b75b45f19fdcd160f925634
beapc_18 <beatrix.pc@gmail.com>
2020-03-17 18:08:26 +0100

    #259 Scripts for make and copy backups

--- src/db/databaseBackup.sh
+++ src/db/databaseBackup.sh
@@ -6,8 +6,5 @@ mongodump --host "127.0.0.1:27017" --out /home/cvg_anno/database_backups/$(date 
 # Request for merging overlapping actions
 curl -X POST http://localhost:8888/api/action/mergeActions
 
-#Copy to cvg25
-
-# Remove backup
-rm -rf /home/cvg_anno/database_backups/*
-
+# Remove backups older than 2 days
+find /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups -mtime +1 -delete
