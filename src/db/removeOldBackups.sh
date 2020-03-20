1e1059ebcb219fab0ec0e2c4198531a50064828b
beapc_18 <beatrix.pc@gmail.com>
2020-03-20 17:28:42 +0100

    #259 Scripts for make and copy backups

--- src/db/removeOldBackups.sh
+++ src/db/removeOldBackups.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # Remove backups older than 7 days
-sudo find /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups -mtime +6 -delete
-
-# Copy backups from server
-scp -P 222 -r 188.138.127.15:/home/cvg_anno/database_backups/* /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups
+find /home/cvg_anno/Data/datasets/actions_in_kitchens/database_backups -mtime +6 -delete
