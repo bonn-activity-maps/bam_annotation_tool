9f217c91217a449b44f1ddd14e790eebf7e489e9
beapc_18 <beatrix.pc@gmail.com>
2020-04-24 16:35:11 +0200

    #278 Finished interpolate by label for poses in AIK

--- src/api.py
+++ src/api.py
@@ -419,7 +419,6 @@ def interpolate_annotation():
     if len(start_frames) > 1 and all(x == start_frames[0] for x in start_frames):
         start_frames = [start_frames[0]]
 
-    # print('start_frames:', start_frames)
     # Use old interpolate if there is only 1 keypoint to interpolate, use new one if it's a poseAIK (>1kp)
     if len(start_frames) == 1:
         object1 = Object(req_data['uidObject'], req_data['objectType'], dataset_type=req_data['datasetType'],
