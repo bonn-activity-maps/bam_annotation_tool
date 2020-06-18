083897f6c3e0b53da4f949fc45b5158b1185d893
dario <dari1495@gmail.com>
2020-06-18 17:40:36 +0200

    Update changed selector for user statistics. Updated statistics shown for user. Prepared frontend for different statistics.

--- src/static/javascript/controllers/adminUserActionsController.js
+++ src/static/javascript/controllers/adminUserActionsController.js
@@ -7,12 +7,24 @@ angular.module('CVGTool')
         function ($scope, $rootScope, $state, adminUserActionsSrvc, adminDatasetsSrvc, adminUsersSrvc, navSrvc, $mdDialog) {
         $scope.listOfActions = [];
         $scope.listOfDatasets = [];
+        $scope.filteredListOfDatasets = [];
         $scope.listOfUsers = [];
+        $scope.filteredListOfUsers = [];
         $scope.selectedItem = {
             name: 'none',
             type: 'none',
             assignedTo: 'none'
         };
+
+        // Form variables to store selected dataset and user
+        $scope.selectedDataset = {
+            name: "None",
+            type: "None"
+        }
+        $scope.selectedUser = {
+            name: "None",
+            assignedTo: []
+        }
 
         $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
         $scope.data = [300, 500, 100];
@@ -29,6 +41,11 @@ angular.module('CVGTool')
         // Update list of datasets variable
         var updateListOfDatasets = function(datasets) {
             $scope.listOfDatasets = datasets;
+            $scope.listOfDatasets.push({
+                name: "None",
+                type: "None"
+            })
+            $scope.filteredListOfDatasets = $scope.listOfDatasets;
         };
 
         // Retrieve list of users in the system
@@ -39,59 +56,123 @@ angular.module('CVGTool')
         // Update list of users variable
         var updateListOfUsers = function(users) {
             $scope.listOfUsers = users;
+            $scope.listOfUsers.push({
+                name: "None",
+                assignedTo: []
+            })
+            $scope.filteredListOfUsers = $scope.listOfUsers;
         };
 
-        // Function to select a dataset and show its actions
+        // Function to select a dataset
         $scope.selectDataset = function(item) {
-            $scope.selectedItem = item;
-            $scope.getInfoOfActions();
+            // Reset list
+            $scope.filteredListOfUsers = [];
+            let j = 0;
+            // Filter list of users by dataset
+            // If "None" selected, admin wants to see all users
+            if ($scope.selectedDataset !== null && $scope.selectedDataset.name !== "None"){
+                // For every user, check its datasets
+                for(let i = 0; i < $scope.listOfUsers.length; i++) {
+                    // Check if user is assigned to selected dataset
+                    for (let k = 0; k < $scope.listOfUsers[i].assignedTo.length; k++) {
+                        // If selected dataset is in list of datasets assigned to user, add to filtered list and go on
+                        if ($scope.listOfUsers[i].assignedTo[k] === $scope.selectedDataset.name){
+                            $scope.filteredListOfUsers[j] = $scope.listOfUsers[i];
+                            j++;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                $scope.filteredListOfUsers = $scope.listOfUsers;
+            }
+            // $scope.selectedItem = item;
+            // $scope.getInfoOfActions();
         };
 
-        // Function to select a dataset and show its actions
+        // Function to select a user
         $scope.selectUser = function(item) {
-            $scope.selectedItem = item;
-            $scope.getInfoOfActionsByUser();
-        };
-
-        // Function to deselect a dataset and stop showing its actions
-        $scope.deselectItem = function() {
-            $scope.selectedItem = {
-                name: 'none',
-                type: 'none',
-                assignedTo: 'none'
-            };
-            $scope.undrawCharts();
-            $scope.getInfoOfActions();
-        };
-
-        // Function to retrieve from the server all information from the actions stored there
-        $scope.getInfoOfActions = function() {
-            if ($scope.selectedItem.name.localeCompare('none') === 0){
-                $scope.listOfActions = [];
+            // Reset list
+            $scope.filteredListOfDatasets = [];
+            let j = 0;
+            // Filter list of users by dataset
+            // If "None" selected, admin wants to see all datasets
+            if ($scope.selectedUser !== null && $scope.selectedUser.name !== "None"){
+                // Add only datasets assigned to user
+                for(let i = 0; i < $scope.selectedUser.assignedTo.length; i++) {
+                    // If dataset is in list of datasets assigned to user, add to filtered list and go on
+                        for (let k = 0; k < $scope.listOfDatasets.length; k++) {
+                            if ($scope.listOfDatasets[k].name === $scope.selectedUser.assignedTo[i]) {
+                                $scope.filteredListOfDatasets[j] = $scope.listOfDatasets[k];
+                                j++;
+                                break;
+                            }
+                        }
+                }
             } else {
-                adminUserActionsSrvc.getUserActions($scope.selectedItem.name, $scope.selectedItem.type, showListOfActions, sendMessage)
+                $scope.filteredListOfDatasets = $scope.listOfDatasets;
             }
         };
 
-        // Function to retrieve from the server all information from the actions by user
-        $scope.getInfoOfActionsByUser = function() {
-            if ($scope.selectedItem.name.localeCompare('none') === 0){
-                $scope.listOfActions = [];
-            } else {
-                adminUserActionsSrvc.getUserActionsByLogin($scope.selectedItem.name, showListOfActions, sendMessage)
-            }
-        };
-
-        // Function to update the list of actions
-        var showListOfActions = function (msg) {
-            // $scope.listOfActions = list;
-            $scope.drawCharts(msg.labels, msg.data);
-        };
+        // Legacy chart code, left here because it pulls everything from DB and it can be useful
+        // // Function to retrieve from the server all information from the actions stored there
+        // $scope.getInfoOfActions = function() {
+        //     if ($scope.selectedItem.name.localeCompare('none') === 0){
+        //         $scope.listOfActions = [];
+        //     } else {
+        //         adminUserActionsSrvc.getUserActions($scope.selectedItem.name, $scope.selectedItem.type, showListOfActions, sendMessage)
+        //     }
+        // };
+        //
+        // // Function to update the list of actions
+        // var showListOfActions = function (msg) {
+        //     // $scope.listOfActions = list;
+        //     $scope.drawCharts(msg.labels, msg.data);
+        // };
 
         // Send message to toast
         var sendMessage = function(type, msg, finishType) {
             $rootScope.$broadcast('sendMsg', {'type': type, 'msg': msg, 'finishType': finishType});
         };
+
+        $scope.loadStatistics = function() {
+            // Lookup data depending on selection
+            // Both user and dataset selected
+            if ($scope.selectedUser.name !== "None" && $scope.selectedDataset.name !== "None") {
+                console.log("User and dataset selected")
+            }
+            // Only dataset selected
+            else if($scope.selectedUser.name === "None" && $scope.selectedDataset.name !== "None") {
+                console.log("Only dataset selected")
+            }
+            // Only user selected
+            else if($scope.selectedUser.name !== "None" && $scope.selectedDataset.name === "None") {
+                console.log("Only user selected");
+                adminUserActionsSrvc.getUserActionsByLogin($scope.selectedUser.name, getActionsCallback, sendMessage);
+            }
+            // None selected
+            else if($scope.selectedUser.name === "None" && $scope.selectedDataset.name === "None") {
+                console.log("Nothing selected");
+                sendMessage("Warning", "Please select some data to visualize.")
+            }
+        }
+
+        let getActionsCallback = function(response) {
+            $scope.drawCharts(response.labels, response.data);
+        }
+
+        $scope.resetSelections = function() {
+            $scope.selectedDataset = {
+                name: "None",
+                type: "None"
+            }
+            $scope.selectedUser = {
+                name: "None",
+                assignedTo: []
+            }
+            $scope.filteredListOfDatasets = $scope.listOfDatasets;
+            $scope.filteredListOfUsers = $scope.listOfUsers;
+        }
 
         /// CHART CODE ///
         // $scope.drawCharts = function(labels, data) {
@@ -155,6 +236,6 @@ angular.module('CVGTool')
         $scope.getListOfDatasets();
         $scope.getListOfUsers();
         $scope.getInfoOfActions();
-        $scope.drawCharts($scope.labels, $scope.data);
+        // $scope.drawCharts($scope.labels, $scope.data);
 
     }]);
