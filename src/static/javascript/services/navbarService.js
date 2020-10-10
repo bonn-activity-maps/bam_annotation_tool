6ce04e7c1344573c593e206dc284997cb834cd58
dario <dari1495@gmail.com>
2020-10-10 13:20:15 +0200

    Update version

--- src/static/javascript/services/navbarService.js
+++ src/static/javascript/services/navbarService.js
@@ -2,7 +2,7 @@ angular.module('CVGTool')
     .factory('navSrvc', function($state, $rootScope, $http, $httpParamSerializer) {
 
         // Actual version of the tool, THIS IS THE MAIN VARIABLE
-        var toolVersion = "2.1";
+        var toolVersion = "2.2";
 
         // Function to send message to tell the controller to update
         var updateSessionData = function() {
